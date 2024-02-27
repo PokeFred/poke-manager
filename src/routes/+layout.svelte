ace0020b904bda897dcf0bee29bf9d54b291e4ba
PokeFred <Cedric.paelmke@gmx.de>
2024-02-27 21:34:39 +0100

    add skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
     import "../app.css"
 </script>
 
-<slot />
+<body data-theme="skeleton" class="dark">
+    <slot />
+</body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
+import { skeleton } from "@skeletonlabs/tw-plugin"
+import { join as joinPath } from "path"
+
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
         "./src/app.html",
         "./src/routes/**/*.svelte",
         "./src/lib/components/**/*.svelte",
+        joinPath(
+            require.resolve("@skeletonlabs/skeleton"),
+            "../**/*.{html,svelte,js,ts}"
         )
     ],
     theme: {
         extend: {}
     },
     plugins: [
+        skeleton({
+            themes: {
+                preset: ["skeleton"]
+            }
+        })
     ]
 }
