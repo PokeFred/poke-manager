720cce9f88538816327fcedb64046b86ff5b55a5
PokeFred <Cedric.paelmke@gmx.de>
2024-02-27 21:17:22 +0100

    init svelte

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite"
+import { sveltekit } from "@sveltejs/kit/vite"
+
+export default defineConfig({
+    server: {
+        host: "127.0.0.1",
+        port: 3000,
+        strictPort: true
+    },
+    plugins: [sveltekit()]
+})
