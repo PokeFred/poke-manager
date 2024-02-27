cdd299ea08357e31cc486d87e62cb134e56b0b75
PokeFred <Cedric.paelmke@gmx.de>
2024-02-27 21:34:23 +0100

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/app.html",
+        "./src/routes/**/*.svelte",
+        "./src/lib/components/**/*.svelte",
+        )
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [
+    ]
+}
